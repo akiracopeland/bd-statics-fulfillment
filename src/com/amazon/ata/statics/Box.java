e525dd352645fb493ea59b74fc1b2c35710c8d24
akiracopeland <akiracopeland713@gmail.com>
2024-10-27 11:29:01 -0700

    quiz done

--- src/com/amazon/ata/statics/Box.java
+++ src/com/amazon/ata/statics/Box.java
@@ -2,6 +2,10 @@ package com.amazon.ata.statics;
 
 public class Box {
 
+    private double length;
+    private double width;
+    private double height;
+
     /**
      * Constructs a Box object if the dimensions are valid
      * @param length - length of box
@@ -10,7 +14,30 @@ public class Box {
      * @throws DimensionValueException if the provided dimensions exceed the max Box size
      */
     public Box(double length, double width, double height) throws DimensionValueException {
+        validateBoxDimensions(length, width, height);
+
+        this.length = length;
+        this.height = height;
+        this.width = width;
 
     }
 
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public static boolean validateBoxDimensions(double height, double width, double length) throws DimensionValueException {
+        if (height > 72 || width > 72 || length > 72) {
+            throw new DimensionValueException("Provided Dimensions are illegal");
+        }
+        return true;
+    }
 }
